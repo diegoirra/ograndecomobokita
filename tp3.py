f3590ba458571b32868e71a10bab138e52235803
diegoirra <dgirra@hotmail.com>
2021-11-04 18:44:20 -0300

    changed tp2 main to tp2 file, created tp3

---
+++ tp3.py
@@ -0,0 +1,56 @@
+import sys
+
+
+vertices = set()
+edges = []
+
+if len(sys.argv) != 2:
+    print()
+    print("ERROR: you need to pass a file as an argument")
+    exit(-1)
+
+file = sys.argv[1]
+
+print("        START")
+print()
+print("Reading depositos from file:", file)
+print()
+
+with open(file) as f:
+    lines = f.readlines()
+    for line in lines:
+        v,u,w = line.strip().split(',')
+        vertices.add(v)
+        vertices.add(u)
+        edge = (v,u)
+        edges.append([edge, int(w)])
+
+vertices_list = list(vertices)
+vertices_list.sort()
+print("Edges",edges)
+print("Vertices", vertices_list)
+print()
+print("File is good. Continuing...")
+print()
+
+
+## do stuff
+
+
+
+
+if __name__ == '__main__':
+    
+    
+    
+    pass
+
+
+
+
+
+
+
+
+
+
